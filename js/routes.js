ae8209f63e5adee5fd09b7bced75dbec558bff55
yocoHM <yocoyanihernandez@gmail.com>
2016-11-07 17:14:50 -0600

    se agregó el cambio de contenido dinámico con AngularJS y se agregaron nuevas vistas

---
+++ js/routes.js
@@ -0,0 +1,33 @@
+var app = angular.module("dashboard",['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+    when('/transacciones',{
+      templateUrl: 'transacciones.html',
+      controller: 'MainCtrl'
+    }).
+    when('/reportes',{
+      templateUrl: 'reportes.html',
+      controller: 'MainCtrl'
+    }).
+    when('/graficas',{
+      templateUrl: 'graficas.html',
+      controller: 'MainCtrl'
+    }).
+    when('/perfil',{
+      templateUrl: 'perfil.html',
+      controller: 'MainCtrl'
+    }).
+    when('/nuevaTransaccion',{
+      templateUrl: 'nuevaTransaccion.html',
+      controller: 'MainCtrl'
+    }).
+    otherwise({redirectTo: '/transacciones'})
+}])
+
+.controller('MainCtrl',['$scope','$http', function($scope, $http){
+  $http.get('services.json').then(function(response){
+    $scope.services = response.data;
+  });
+}])
+;
